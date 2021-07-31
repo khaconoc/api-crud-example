fe77cfb7947f94518b20ed49e0b03c7ae39fd555
kha <hoangkha.tp@gmail.com>
2021-07-31 23:49:59 +0700

    init project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.apicrudexample;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiCrudExampleApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiCrudExampleApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.apicrudexample;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiCrudExampleApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
