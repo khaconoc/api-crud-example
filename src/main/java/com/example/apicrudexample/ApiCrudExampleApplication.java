f9cf7dccd651134b1836b95601acec7bee7dca10
kha <hoangkha.tp@gmail.com>
2021-07-31 23:52:25 +0700

    init project

--- src/main/java/com/example/apicrudexample/ApiCrudExampleApplication.java
+++ src/main/java/com/example/apicrudexample/ApiCrudExampleApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.apicrudexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ApiCrudExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ApiCrudExampleApplication.class, args);
 	}
 
 }
