4d823385bda96f5c85fe58f93639ed50caa49774
kha <hoangkha.tp@gmail.com>
2021-08-01 23:14:25 +0700

    allow all cross

--- src/main/java/com/example/apicrudexample/controller/HomeController.java
+++ src/main/java/com/example/apicrudexample/controller/HomeController.java
@@ -1,12 +1,21 @@
 package com.example.apicrudexample.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class HomeController {
-    @GetMapping("/")
-    public String testProcess() {
-        return "api running";
+//    @GetMapping("/")
+//    public String testProcess() {
+//        return "index";
+//    }
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+        return "index";
     }
 }
