9dbb5a17811fd6e0e63fb37b083a3c4e1e3f4af2
kha <hoangkha.tp@gmail.com>
2021-08-03 13:33:33 +0700

    allow all cross

--- src/main/java/com/example/apicrudexample/ApiCrudExampleApplication.java
+++ src/main/java/com/example/apicrudexample/ApiCrudExampleApplication.java
@@ -2,12 +2,23 @@ package com.example.apicrudexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiCrudExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCrudExampleApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("**");
+			}
+		};
 	}
 
 }
--- src/main/java/com/example/apicrudexample/configuration/CrossConfig.java
+++ src/main/java/com/example/apicrudexample/configuration/CrossConfig.java
@@ -1,17 +1,21 @@
-package com.example.apicrudexample.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class CrossConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-
-}
+//package com.example.apicrudexample.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//@Configuration
+//@EnableWebMvc
+//public class CrossConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+//}
